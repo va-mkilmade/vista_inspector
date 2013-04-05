90a377c498c08a479e0b15cd7b9902c1efb9a707
Mike Kilmade <mike.kilmade@va.gov>
2013-04-05 16:02:53 -0400

    JSLint improvements

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,5 @@
 // url for reference "http://10.1.17.116:57772/csp/cpm/vi.paths.cls";
-
+"use strict";
 var deflt = {"ip" : "10.1.17.116", "port" : "57772", "namespace" : "cpm"};
 var site = {"ip" : "", "port" : "", "namespace" : ""};
 
@@ -10,150 +10,159 @@ $(document).bind("mobileinit", function() {
 	$.mobile.loader.prototype.options.theme = "b";
 });
 
+function buildUrl(area) {
+    return "http://" + site.ip + ":" + site.port + "/csp/" + site.namespace + "/vi." + area + ".cls";
+}
+
+function triggerCreate(event) {
+    $("#" + event.data.value).trigger("create");
+}
+
+function buildPaths() {
+    $("#vi-content").html("");
+    var url = buildUrl("paths");
+    $.getJSON(url, function(data) {
+        // build html for content role
+        var x, key, i , j, text, id, y, areaPageId, areaContentId, func;
+        x = '<ul data-role="listview" data-inset="true">';
+        for (i = 0; i < data.areas.length; i++) {
+            areaPageId = 'vi-' + data.areas[i].id;
+            areaContentId = 'vi-' + data.areas[i].id + '-content';
+            x = x + '<li><a href="#' + areaPageId + '">' + data.areas[i].text + '</a></li>';
+            if (data.areas[i].hasOwnProperty("items")) {
+                y = '<ul data-role="listview" data-inset="true">';
+                for (j = 0; j < data.areas[i].items.length; j++) {
+                    text = "";
+                    id = "";
+                    for (key in data.areas[i].items[j]) {
+                        if (data.areas[i].items[j].hasOwnProperty(key)) {
+                            if (key === "text") {
+                                text = data.areas[i].items[j][key];
+                            } else {
+                                if (key === "id") {
+                                    id = "vi-" + data.areas[i].items[j][key];
+                                } // if id
+                            } // else
+                        }// hasOwnProperty (key)
+                    } // for key
+                y = y + '<li><a id="' + id + '-item" href="#' + id + '">' + text + '</a></li>';
+                } // for j
+            y = y + "</ul>";
+            $("#" + areaContentId).html(y);
+            $('#' + areaPageId).on('pagebeforeshow', {value : areaContentId}, triggerCreate);
+            } // hasOwnProperty (items)
+        } // for i
+        x = x + "</ul>";
+        $("#vi-content").html(x);
+        $("#vi-content").trigger('create');
+        // bind functions after html inserted
+        for (i = 0; i < data.areas.length; i++) {
+            if (data.areas[i].hasOwnProperty("items")) {
+                for (j = 0; j < data.areas[i].items.length; j++) {
+                    func = "";
+                    id = "";
+                    for (key in data.areas[i].items[j]) {
+                        if (data.areas[i].items[j].hasOwnProperty(key)) {
+                            if (key === "function") {
+                                func = data.areas[i].items[j][key];
+                            } else {
+                                if (key === "id") {
+                                    id = "vi-" + data.areas[i].items[j][key] + '-item';
+                                } // if id
+                            } // else
+                        }// if hasOwnProperty (key)
+                    } // for key
+                if (window[func]) {
+                    $("#" + id ).click(window[func]);
+                } // click binding
+                } // for j
+            } // hasOwnProperty (items)
+        } // for i
+    });//getJSON
+}
+
+function onGo() {
+    site.ip = $("#vi-ip").val();
+    if (site.ip === "") {
+        site.ip = deflt.ip;
+        $("#vi-ip").val(site.ip);
+    }
+    site.port = $("#vi-port").val();
+    if (site.port === "") {
+        site.port = deflt.port;
+        $("#vi-port").val(site.port);
+    }
+    site.namespace = $("#vi-namespace").val();
+    if (site.namespace === "") {
+        site.namespace = deflt.namespace;
+        $("#vi-namespace").val(site.namespace);
+    }
+    buildPaths();
+    $.mobile.changePage("#vi-main");
+}
+
 $("document").ready(function() {
 	$("#vi-ip-go-btn").click(onGo);
 });
 
-function onGo() {
-	site.ip = $("#vi-ip").val();
-	if (site.ip === "") {
-		site.ip = deflt.ip;
-		$("#vi-ip").val(site.ip);
-	}
-	site.port = $("#vi-port").val();
-	if (site.port === "") {
-		site.port = deflt.port;
-		$("#vi-port").val(site.port);
-	}
-	site.namespace = $("#vi-namespace").val();
-	if (site.namespace === "") {
-		site.namespace = deflt.namespace;
-		$("#vi-namespace").val(site.namespace);
-	}
-	buildPaths();
-	$.mobile.changePage("#vi-main");
+function buildRow(caption, value) {
+    return "<tr><td>" + caption + "</td><td>" + value + "</td></tr>";
 }
-
-function buildPaths() {
-	$("#vi-content").html("");
-	var url = buildUrl("paths");
-	$.getJSON(url, function(data) {
-		// build html for content role
-		var x = '<ul data-role="listview" data-inset="true">';
-		for (var i = 0; i < data.areas.length; i++) {
-			var areaPageId = 'vi-' + data.areas[i].id;
-			var areaContentId = 'vi-' + data.areas[i].id + '-content';
-			x = x + '<li><a href="#' + areaPageId + '">' + data.areas[i].text + '</a></li>';
-			if (data.areas[i].hasOwnProperty("items")) {
-				var y = '<ul data-role="listview" data-inset="true">';
-				for (var j = 0; j < data.areas[i].items.length; j++) {
-					var text = "";
-					var id = "";
-					for (key in data.areas[i].items[j]) {
-						if (data.areas[i].items[j].hasOwnProperty(key)) {
-							if (key === "text") {
-								text = data.areas[i].items[j][key];
-							} else {
-								if (key === "id") {
-									id = "vi-" + data.areas[i].items[j][key];
-								} // if id
-							} // else
-						}// hasOwnProperty (key)
-					} // for key
-				y = y + '<li><a id="' + id + '-item" href="#' + id + '">' + text + '</a></li>';
-				} // for j
-			y = y + "</ul>"
-			$("#" + areaContentId).html(y);
-			$('#' + areaPageId).on('pagebeforeshow', {value : areaContentId}, function(event) {
-			    $("#" + event.data.value).trigger("create");
-			});
-			} // hasOwnProperty (items)
-		};// for i
-		x = x + "</ul>";
-		$("#vi-content").html(x);
-		$("#vi-content").trigger('create');
-		// bind functions after html inserted
-		for (var i = 0; i < data.areas.length; i++) {
-			if (data.areas[i].hasOwnProperty("items")) {
-				for (var j = 0; j < data.areas[i].items.length; j++) {
-					var func = "";
-					var id = "";
-					for (key in data.areas[i].items[j]) {
-						if (data.areas[i].items[j].hasOwnProperty(key)) {
-							if (key === "function") {
-								func = data.areas[i].items[j][key];
-							} else {
-								if (key === "id") {
-									id = "vi-" + data.areas[i].items[j][key] + '-item';
-								} // if id
-							} // else
-						}// if hasOwnProperty (key)
-					} // for key
-				if (window[func]) {
-					$("#" + id ).click(window[func]);
-				} // click binding
-				} // for j
-			} // hasOwnProperty (items)
-		} // for i
-	});//getJSON
-}
-
-function buildUrl(area) {
-	return "http://" + site.ip + ":" + site.port + "/csp/" + site.namespace + "/vi." + area + ".cls";
-}
-
 function doTaskMan() {
 	$("#vi-taskman-results").html("");
 	var url = buildUrl("taskman");
 	$.getJSON(url, function(data) {
-		var x = ""
-		x = x + "Run date/time: " + data.date
+		var x, node, value, i, key;
+		x = "";
+		x = x + "Run date/time: " + data.date;
 		x = x + "<table><thead><tr><th>TM OS Item</th><th>Status</th></tr></thead>";
 		// insert os env properties
-		x = x + "<tr><td>OS</td><td>" + data.os.os + "</td></tr>";
-		x = x + "<tr><td>Volume Set</td><td>" + data.os.volset + "</td></tr>";
-		x = x + "<tr><td>Cpu:Volume Pair</td><td>" + data.os.cpuvol + "</td></tr>";
-		x = x + "<tr><td>TM Files UCI,Volume Set</td><td>" + data.os.ucivol + "</td></tr>";
-		x = x + "<tr><td>Log Tasks?</td><td>" + data.os.logtasks + "</td></tr>";
+		x = x + buildRow("OS",data.os.os);
+		x = x + buildRow("Volume Set",data.os.volset);
+		x = x + buildRow("Cpu:Volume Pair<",data.os.cpuvol);
+		x = x + buildRow("TM Files UCI,VolSet",data.os.ucivol);
+		x = x + buildRow("Log Tasks?",data.os.logtasks);
 		if (data.os.hasOwnProperty("partsize")) {
-		x = x + "<tr><td>Task Partition Size</td><td>" + data.os.partsize + "</td></tr>";
+					x = x + buildRow("TM Partition Size",data.os.partsize);
 		}
-		x = x + "<tr><td>Submgr Retention</td><td>" + data.os.retention + "</td></tr>";
-		x = x + "<tr><td>Min# Submgrs</td><td>" + data.os.minsubmgrs + "</td></tr>";
-		x = x + "<tr><td>Hang Between New Jobs</td><td>" + data.os.hangtime + "</td></tr>";
-		x = x + "<tr><td>TM Run Type</td><td>" + data.os.runtype + "</td></tr>";
+		x = x + buildRow("Submgr Retention",data.os.retention);
+		x = x + buildRow("Min# Submgrs",data.os.minsubmgrs);
+		x = x + buildRow("Hang Between New Jobs",data.os.hangtime);
+		x = x + buildRow("TM Run Type",data.os.runtype);
 		if (data.os.hasOwnProperty("vaxenv")) {
-			x = x + "<tr><td>TM VAX enviroment</td><td>" + data.os.vaxenv + "</td></tr>";
+					x = x + buildRow("TM VAX enviroment",data.os.vaxenv);
 		}
 		if (data.os.hasOwnProperty("loadbalancing")) {
-			x = x + "<tr><td>TM Load Balancing Type</td><td>" + data.os.loadbalancing + "</td></tr>";
+					x = x + buildRow("TM Load Balancing Type",data.os.loadbalancing);
 		}
 		if (data.os.hasOwnProperty("balanceinterval")) {
-			x = x + "<tr><td>Balance Interval</td><td>" + data.os.balanceinterval + "</td></tr>";
+					x = x + buildRow("Balance Interval",data.os.balanceinterval);
 		}
 		x = x + "<tr><td>Logons Inhibited?</td><td>" + data.os.inhibited + "</td></tr>";
 		x = x + "<tr><td>TM Job Limit</td><td>" + data.os.limit + "</td></tr>";
 		if (data.os.hasOwnProperty("max")) {
-			x = x + "<tr><td>Max sign-ons</td><td>" + data.os.max + "</td></tr>";
+					x = x + buildRow("Max sign-ons",data.os.max);
 		}
-		x = x + "<tr><td>Active Jobs</td><td>" + data.os.activejobs + "</td></tr>";
-		x = x + "<tr><td>Linked VolSets</td><td>" + data.os.linkedvolsets + "</td></tr>";
+		x = x + buildRow("Active Jobs",data.os.activejobs);
+		x = x + buildRow("Linked VolSets",data.os.linkedvolsets);
 		// insert TM global node statuses
 		x = x + "<thead><tr><th>TM Global</th><th>Status</th></tr></thead>";
-		for (var i = 0; i < data.nodes.length; i++) {
+		node = "";
+		value = "";
+		for (i = 0; i < data.nodes.length; i++) {
 			for (key in data.nodes[i]) {
 				if (data.nodes[i].hasOwnProperty(key)) {
 					if (key === "node") {
-						var node = data.nodes[i][key];
+						node = data.nodes[i][key];
 					} else {
 						if (key === "value") {
-							var value = data.nodes[i][key];
+							value = data.nodes[i][key];
 						} // if value
 					} // if node
 				} // if hasOwnProperty
 			} // for key
-			x = x + "<tr><td>" + node + "</td><td>" + value + "</td></tr>";
-		}; // for i
+			x = x + buildRow(node,value);
+		} // for i
 		x = x + "</table>";
 		$("#vi-taskman-results").html(x);
 	}); //getJSON
