fcb9fbbb8bdc397e1ab042b783b7d82f79a97a75
Mike Kilmade <mike.kilmade@va.gov>
2013-04-10 14:47:14 -0400

    Add jasmine unit testing and refactoring to support jasmine testing

--- www/js/index.js
+++ www/js/index.js
@@ -18,85 +18,104 @@ function triggerCreate(event) {
     $("#" + event.data.value).trigger("create");
 }
 
+function buildArea(node) {
+    var j, id, text, key, y;
+    y ="";
+    if (node.hasOwnProperty("items")) {
+        y = '<ul data-role="listview" data-inset="true">';
+        for (j = 0; j < node.items.length; j++) {
+            text = "";
+            id = "";
+            for (key in node.items[j]) {
+                if (node.items[j].hasOwnProperty(key)) {
+                    if (key === "text") {
+                        text = node.items[j][key];
+                    } else {
+                         if (key === "id") {
+                             id = "vi-" + node.items[j][key];
+                         } // if id
+                    } // else
+                }// hasOwnProperty (key)
+             } // for key
+             y = y + '<li><a id="' + id + '-item" href="#' + id + '">' + text + '</a></li>';
+         } // for j
+      } // hasOwnProperty (items)
+      y = y + "</ul>";
+      return y;
+}
+
+function bindFunctions(node) {
+    var j, func, id, key;
+    if (node.hasOwnProperty("items")) {
+        for ( j = 0; j < node.items.length; j++) {
+            func = "";
+            id = "";
+            for (key in node.items[j]) {
+                if (node.items[j].hasOwnProperty(key)) {
+                    if (key === "function") {
+                        func = node.items[j][key];
+                    } else {
+                        if (key === "id") {
+                            id = "vi-" + node.items[j][key] + '-item';
+                        } // if id
+                    } // else
+                }// if hasOwnProperty (key)
+            }// for key
+            if (window[func]) {
+                $("#" + id).click(window[func]);
+            } // click binding
+        } // for j
+    } // hasOwnProperty (items)
+}
+
 function buildPaths() {
     $("#vi-content").html("");
-    var url = buildUrl("paths");
+    var url;
+    url = buildUrl("paths");
     $.getJSON(url, function(data) {
         // build html for content role
-        var x, key, i , j, text, id, y, areaPageId, areaContentId, func;
+        var x, index, areaPageId, areaContentId, node;
         x = '<ul data-role="listview" data-inset="true">';
-        for (i = 0; i < data.areas.length; i++) {
-            areaPageId = 'vi-' + data.areas[i].id;
-            areaContentId = 'vi-' + data.areas[i].id + '-content';
-            x = x + '<li><a href="#' + areaPageId + '">' + data.areas[i].text + '</a></li>';
-            if (data.areas[i].hasOwnProperty("items")) {
-                y = '<ul data-role="listview" data-inset="true">';
-                for (j = 0; j < data.areas[i].items.length; j++) {
-                    text = "";
-                    id = "";
-                    for (key in data.areas[i].items[j]) {
-                        if (data.areas[i].items[j].hasOwnProperty(key)) {
-                            if (key === "text") {
-                                text = data.areas[i].items[j][key];
-                            } else {
-                                if (key === "id") {
-                                    id = "vi-" + data.areas[i].items[j][key];
-                                } // if id
-                            } // else
-                        }// hasOwnProperty (key)
-                    } // for key
-                y = y + '<li><a id="' + id + '-item" href="#' + id + '">' + text + '</a></li>';
-                } // for j
-            y = y + "</ul>";
-            $("#" + areaContentId).html(y);
+        
+        // build areas
+        for (index = 0; index < data.areas.length; index++) {
+            node = data.areas[index];
+            areaPageId = 'vi-' + node.id;
+            areaContentId = 'vi-' + node.id + '-content';
+            x = x + '<li><a href="#' + areaPageId + '">' + node.text + '</a></li>';
+            $("#" + areaContentId).html(buildArea(node));
             $('#' + areaPageId).on('pagebeforeshow', {value : areaContentId}, triggerCreate);
-            } // hasOwnProperty (items)
+            // bind functions after html inserted
+            bindFunctions(node);
         } // for i
+        
+        // finish up html and apply
         x = x + "</ul>";
         $("#vi-content").html(x);
-        $("#vi-content").trigger('create');
-        // bind functions after html inserted
-        for (i = 0; i < data.areas.length; i++) {
-            if (data.areas[i].hasOwnProperty("items")) {
-                for (j = 0; j < data.areas[i].items.length; j++) {
-                    func = "";
-                    id = "";
-                    for (key in data.areas[i].items[j]) {
-                        if (data.areas[i].items[j].hasOwnProperty(key)) {
-                            if (key === "function") {
-                                func = data.areas[i].items[j][key];
-                            } else {
-                                if (key === "id") {
-                                    id = "vi-" + data.areas[i].items[j][key] + '-item';
-                                } // if id
-                            } // else
-                        }// if hasOwnProperty (key)
-                    } // for key
-                if (window[func]) {
-                    $("#" + id ).click(window[func]);
-                } // click binding
-                } // for j
-            } // hasOwnProperty (items)
-        } // for i
+        $("#vi-content").trigger('create');        
     });//getJSON
 }
 
-function onGo() {
+function buildSite() {
     site.ip = $("#vi-ip").val();
-    if (site.ip === "") {
+    if (site.ip === undefined || site.ip === null || site.ip === "") {
         site.ip = deflt.ip;
         $("#vi-ip").val(site.ip);
     }
     site.port = $("#vi-port").val();
-    if (site.port === "") {
+    if (site.port === undefined || site.port === null || site.port === "") {
         site.port = deflt.port;
         $("#vi-port").val(site.port);
     }
     site.namespace = $("#vi-namespace").val();
-    if (site.namespace === "") {
+    if (site.namespace === undefined || site.namespace === null || site.namespace === "") {
         site.namespace = deflt.namespace;
         $("#vi-namespace").val(site.namespace);
     }
+}
+
+function onGo() {
+    buildSite();
     buildPaths();
     $.mobile.changePage("#vi-main");
 }
@@ -108,61 +127,78 @@ $("document").ready(function() {
 function buildRow(caption, value) {
     return "<tr><td>" + caption + "</td><td>" + value + "</td></tr>";
 }
+function getTaskManPropertiesText(data) {
+    var x = "";
+    // insert os env properties
+    x = x + buildRow("OS", data.os.os);
+    x = x + buildRow("Volume Set", data.os.volset);
+    x = x + buildRow("Cpu:Volume Pair<", data.os.cpuvol);
+    x = x + buildRow("TM Files UCI,VolSet", data.os.ucivol);
+    x = x + buildRow("Log Tasks?", data.os.logtasks);
+    if (data.os.hasOwnProperty("partsize")) {
+        x = x + buildRow("TM Partition Size", data.os.partsize);
+    }
+    x = x + buildRow("Submgr Retention", data.os.retention);
+    x = x + buildRow("Min# Submgrs", data.os.minsubmgrs);
+    x = x + buildRow("Hang Between New Jobs", data.os.hangtime);
+    x = x + buildRow("TM Run Type", data.os.runtype);
+    if (data.os.hasOwnProperty("vaxenv")) {
+        x = x + buildRow("TM VAX enviroment", data.os.vaxenv);
+    }
+    if (data.os.hasOwnProperty("loadbalancing")) {
+        x = x + buildRow("TM Load Balancing Type", data.os.loadbalancing);
+    }
+    if (data.os.hasOwnProperty("balanceinterval")) {
+        x = x + buildRow("Balance Interval", data.os.balanceinterval);
+    }
+    x = x + "<tr><td>Logons Inhibited?</td><td>" + data.os.inhibited + "</td></tr>";
+    x = x + "<tr><td>TM Job Limit</td><td>" + data.os.limit + "</td></tr>";
+    if (data.os.hasOwnProperty("max")) {
+        x = x + buildRow("Max sign-ons", data.os.max);
+    }
+    x = x + buildRow("Active Jobs", data.os.activejobs);
+    x = x + buildRow("Linked VolSets", data.os.linkedvolsets);
+    return x;
+}
+
+function getGlobalStatusesText(data) {
+    var x, node, global, value, index, key;
+    node = "";
+    value = "";
+    x = "";
+    for ( index = 0; index < data.nodes.length; index++) {
+        global = data.nodes[index];
+        for (key in global) {
+            if (global.hasOwnProperty(key)) {
+                if (key === "node") {
+                    node = global[key];
+                } else {
+                    if (key === "value") {
+                        value = global[key];
+                    } // if value
+                } // if node
+            } // if hasOwnProperty
+        }// for key
+        x = x + buildRow(node, value);
+    } // for i
+    return x;
+}
+
 function doTaskMan() {
-	$("#vi-taskman-results").html("");
 	var url = buildUrl("taskman");
 	$.getJSON(url, function(data) {
-		var x, node, value, i, key;
-		x = "";
-		x = x + "Run date/time: " + data.date;
-		x = x + "<table><thead><tr><th>TM OS Item</th><th>Status</th></tr></thead>";
-		// insert os env properties
-		x = x + buildRow("OS",data.os.os);
-		x = x + buildRow("Volume Set",data.os.volset);
-		x = x + buildRow("Cpu:Volume Pair<",data.os.cpuvol);
-		x = x + buildRow("TM Files UCI,VolSet",data.os.ucivol);
-		x = x + buildRow("Log Tasks?",data.os.logtasks);
-		if (data.os.hasOwnProperty("partsize")) {
-					x = x + buildRow("TM Partition Size",data.os.partsize);
-		}
-		x = x + buildRow("Submgr Retention",data.os.retention);
-		x = x + buildRow("Min# Submgrs",data.os.minsubmgrs);
-		x = x + buildRow("Hang Between New Jobs",data.os.hangtime);
-		x = x + buildRow("TM Run Type",data.os.runtype);
-		if (data.os.hasOwnProperty("vaxenv")) {
-					x = x + buildRow("TM VAX enviroment",data.os.vaxenv);
-		}
-		if (data.os.hasOwnProperty("loadbalancing")) {
-					x = x + buildRow("TM Load Balancing Type",data.os.loadbalancing);
-		}
-		if (data.os.hasOwnProperty("balanceinterval")) {
-					x = x + buildRow("Balance Interval",data.os.balanceinterval);
-		}
-		x = x + "<tr><td>Logons Inhibited?</td><td>" + data.os.inhibited + "</td></tr>";
-		x = x + "<tr><td>TM Job Limit</td><td>" + data.os.limit + "</td></tr>";
-		if (data.os.hasOwnProperty("max")) {
-					x = x + buildRow("Max sign-ons",data.os.max);
-		}
-		x = x + buildRow("Active Jobs",data.os.activejobs);
-		x = x + buildRow("Linked VolSets",data.os.linkedvolsets);
+		var x = "";
+        x = x + "Run date/time: " + data.date;
+        
+        // insert properties
+        x = x + "<table><thead><tr><th>TM OS Item</th><th>Status</th></tr></thead>";		
+		x = x + getTaskManPropertiesText(data);
+
 		// insert TM global node statuses
 		x = x + "<thead><tr><th>TM Global</th><th>Status</th></tr></thead>";
-		node = "";
-		value = "";
-		for (i = 0; i < data.nodes.length; i++) {
-			for (key in data.nodes[i]) {
-				if (data.nodes[i].hasOwnProperty(key)) {
-					if (key === "node") {
-						node = data.nodes[i][key];
-					} else {
-						if (key === "value") {
-							value = data.nodes[i][key];
-						} // if value
-					} // if node
-				} // if hasOwnProperty
-			} // for key
-			x = x + buildRow(node,value);
-		} // for i
+		x = x + getGlobalStatusesText(data);
+		
+		// attach and finish up
 		x = x + "</table>";
 		$("#vi-taskman-results").html(x);
 	}); //getJSON
--- www/spec/index.js
+++ www/spec/index.js
@@ -1,67 +1,87 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-describe('app', function() {
-    describe('initialize', function() {
-        it('should bind deviceready', function() {
-            runs(function() {
-                spyOn(app, 'onDeviceReady');
-                app.initialize();
-                helper.trigger(window.document, 'deviceready');
-            });
-
-            waitsFor(function() {
-                return (app.onDeviceReady.calls.length > 0);
-            }, 'onDeviceReady should be called once', 500);
-
-            runs(function() {
-                expect(app.onDeviceReady).toHaveBeenCalled();
-            });
-        });
+describe("VistA Inspector", function() {
+ 
+    // Put function in ECMA Script 5 "strict" operating context
+    "use strict";
+   
+   function getIp() { return "10.1.17.116"; }
+   function getUrl() { return "http://10.1.17.116:57772/csp/cpm/vi.paths.cls"; }
+    
+    beforeEach(function() {
+        buildSite();
     });
-
-    describe('onDeviceReady', function() {
-        it('should report that it fired', function() {
-            spyOn(app, 'receivedEvent');
-            app.onDeviceReady();
-            expect(app.receivedEvent).toHaveBeenCalledWith('deviceready');
-        });
+    
+    it("deflt should be defined", function() {
+        expect(deflt).toBeDefined();
     });
-
-    describe('receivedEvent', function() {
-        beforeEach(function() {
-            var el = document.getElementById('stage');
-            el.innerHTML = ['<div id="deviceready">',
-                            '    <p class="event listening">Listening</p>',
-                            '    <p class="event received">Received</p>',
-                            '</div>'].join('\n');
+     
+    it("site.ip should be equal to " + getIp(), function() {
+        expect(site.ip).toEqual(getIp());
+    }); 
+    
+    it("url should equal " + getUrl(), function() {
+        expect(buildUrl("paths")).toContain(getUrl());
+    }); 
+    
+    it("Kernel items should contain TaskMan from Async call", function() {
+        buildSite();
+        var url, output;
+        url = buildUrl("paths");
+        output = "";
+        $.getJSON(url, function(data) {
+            var index, node;        
+            // build areas
+            for (index = 0; index < data.areas.length; index++) {
+                node = data.areas[index];
+                if (node.id === "kernel") {
+                    output = buildArea(node);
+                    break;                   
+                }
+             }
         });
+    
+        waitsFor(function() {
+            return ( output.length > 0 );
+            }, "obtaining paths data never completed", 10000);
 
-        it('should hide the listening element', function() {
-            app.receivedEvent('deviceready');
-            var displayStyle = helper.getComputedStyle('#deviceready .listening', 'display');
-            expect(displayStyle).toEqual('none');
+        runs(function () {
+            expect(output).toContain("TaskMan");
         });
+     });
 
-        it('should show the received element', function() {
-            app.receivedEvent('deviceready');
-            var displayStyle = helper.getComputedStyle('#deviceready .received', 'display');
-            expect(displayStyle).toEqual('block');
+
+    it("TaskMan data should be defined and formated correctly", function() {
+        var props, globals, url;
+        
+        props = "";
+        globals = "";
+        url = buildUrl("taskman");
+        
+        $.getJSON(url, function(data) {
+            props = getTaskManPropertiesText(data);
+            globals = getGlobalStatusesText(data);
+        }); //getJSON
+        
+        waitsFor(function() {
+            return ( globals.length > 0 );
+            }, "obtaining TaskMan data never completed", 10000);
+
+        runs(function () {
+            expect(props).toContain("CPM,ROU");
+            expect(globals).toContain("ZTSCH");
         });
-    });
-});
+    }); 
+
+});  
+      
+describe("VistA Inspector Tools", function() {
+ 
+    // Put function in ECMA Script 5 "strict" operating context
+    "use strict";
+   
+   function getRow() { return "<tr><td>caption</td><td>value</td></tr>"; }
+   
+    it("buildRow should be equal to " + getRow(), function() {
+        expect(buildRow("caption", "value")).toEqual(getRow());
+    }); 
+    
+});        
